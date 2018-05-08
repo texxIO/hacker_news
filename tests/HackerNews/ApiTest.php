206e227934b6ae339be088e3a3a67d22c2a90b38
Ionut Mihaescu <ionut.mihaescu@telekom.ro>
2018-05-08 12:35:22 +0300

    phpunit added, one test added for testing

---
+++ tests/HackerNews/ApiTest.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace HackerNews;
+
+    use HackerNews\Helpers\HackerNewsApi;
+    use PHPUnit\Framework\TestCase;
+
+    class ApiTest extends TestCase
+    {
+        public function setUp()
+        {
+            parent::setUp();
+
+        }
+
+        /**
+         *
+         */
+        public function testTrue()
+        {
+            return $this->assertTrue(true);
+        }
+
+
+    }
